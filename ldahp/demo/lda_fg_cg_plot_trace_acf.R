7bacdee17dbac2694f02c7dec75211d811496e38
clintpgeorge <clintpg@ufl.edu>
2013-09-10 15:22:58 -0400

    ACGS and FGS trace plots testing

--- ldahp/demo/lda_fg_cg_plot_trace_acf.R
+++ ldahp/demo/lda_fg_cg_plot_trace_acf.R
@@ -80,7 +80,7 @@ load(rdata.file)
 # Log marginal posterior plots 
 # --------------------------------------------------------------------------------------
 
-postscript(file=log.marginal.posterior.plot, title="log marginal posterior plots", horiz=F)  
+postscript(file=log.marginal.posterior.plot, title="log marginal posterior plots", horiz=F, height=8, width=11.5)  
 par(mfrow = c(2, 2))
 
 x.axis <- (burn.in+1):(burn.in+dim(fg.mdl$lmp)[1])
@@ -99,17 +99,17 @@ dev.off()
 # Log posterior plots 
 # --------------------------------------------------------------------------------------
 
-postscript(file=log.posterior.plot, title="Log posterior plots", horiz=F)  
+postscript(file=log.posterior.plot, title="Log posterior plots", horiz=F, height=8, width=11.5)  
 par(mfrow = c(2, 2))
 
-x.axis <- (burn.in+1):(burn.in+dim(fg.mdl$lmp)[1])
-plot(x.axis, fg.mdl$lmp[,1], type="l", col="blue", ylab="Log posterior", xlab="Iteration", main="Full Gibbs Sampler", lwd=0.4, cex = 2, cex.lab = 1.5, cex.main = 1.2) # ylim=c(59500, 60300), 
-plot(x.axis, cg.mdl$lmp[,1], type="l", col="black", ylab="Log posterior", xlab="Iteration", main="Augmented Collapsed Gibbs Sampler", lwd=0.4, cex = 2, cex.lab = 1.5, cex.main = 1.2) # ylim=c(59500, 60300), 
+x.axis <- (burn.in+1):(burn.in+dim(fg.mdl$lp)[1])
+plot(x.axis, fg.mdl$lp[,1], type="l", col="blue", ylab="Log posterior", xlab="Iteration", main="Full Gibbs Sampler", lwd=0.4, cex = 2, cex.lab = 1.5, cex.main = 1.2, ylim=c(-29400, -28300)) # , 
+plot(x.axis, cg.mdl$lp[,1], type="l", col="black", ylab="Log posterior", xlab="Iteration", main="Augmented Collapsed Gibbs Sampler", lwd=0.4, cex = 2, cex.lab = 1.5, cex.main = 1.2, ylim=c(-29400, -28300)) # ylim=c(59500, 60300), 
 
 # acf on the log marginal posterior 
 
-acf(fg.mdl$lmp[,1], lag.max=100, main="Full Gibbs Sampler", cex = 2, cex.lab = 1.5, cex.main = 3)
-acf(cg.mdl$lmp[,1], lag.max=100, main="Augmented Collapsed Gibbs Sampler", cex = 2, cex.lab = 1.5, cex.main = 3)
+acf(fg.mdl$lp[,1], lag.max=100, main="Full Gibbs Sampler", cex = 2, cex.lab = 1.5, cex.main = 3)
+acf(cg.mdl$lp[,1], lag.max=100, main="Augmented Collapsed Gibbs Sampler", cex = 2, cex.lab = 1.5, cex.main = 3)
 
 dev.off()
 
@@ -120,7 +120,7 @@ dev.off()
 
 # acf computed on the theta counts 
 
-postscript(file=theta.acf.plot, title="theta elements' acf plots", horiz=F)  
+postscript(file=theta.acf.plot, title="theta elements' acf plots", horiz=F, height=8, width=11.5)  
 par(mfrow = c(2,2))
 acf(fg.mdl$theta[1,1,], lag.max=50, main=expression(paste("FGS: ACF for ", theta)[1][1]), lwd=3, cex = 2, cex.lab = 1.5, cex.main = 1.5)
 acf(cg.mdl$theta[1,1,], lag.max=50, main=expression(paste("ACGS: ACF for ", theta)[1][1]), lwd=3, cex = 2, cex.lab = 1.5, cex.main = 1.5)
@@ -130,14 +130,14 @@ acf(cg.mdl$theta[1,8,], lag.max=50, main=expression(paste("ACGS: ACF for ", thet
 dev.off()
 
 # acf computed on the beta counts 
-
-postscript(file=beta.acf.plot, title="beta elements' acf plots", horiz=F)  
+# beta.acf.plot <- "/home/clintpg/results/fg_cg_ea33_beta.eps"
+postscript(file=beta.acf.plot, title="beta elements' acf plots", horiz=F, height=8, width=11.5)  
 par(mfrow = c(2,2))
 acf(fg.mdl$beta[1,1,], lag.max=50, main=expression(paste("FGS: ACF for ", beta)[1][1]), lwd=3, cex = 2, cex.lab = 1.5, cex.main = 1.5)
 acf(cg.mdl$beta[1,1,], lag.max=50, main=expression(paste("ACGS: ACF for ", beta)[1][1]), lwd=3, cex = 2, cex.lab = 1.5, cex.main = 1.5)
 
-acf(fg.mdl$beta[1,4,], lag.max=50, main=expression(paste("FGS: ACF for ", beta)[1][4]), lwd=3, cex = 2, cex.lab = 1.5, cex.main = 1.5)
-acf(cg.mdl$beta[1,4,], lag.max=50, main=expression(paste("ACGS: ACF for ", beta)[1][4]), lwd=3, cex = 2, cex.lab = 1.5, cex.main = 1.5)
+acf(fg.mdl$beta[1,7,], lag.max=50, main=expression(paste("FGS: ACF for ", beta)[1][7]), lwd=3, cex = 2, cex.lab = 1.5, cex.main = 1.5)
+acf(cg.mdl$beta[1,7,], lag.max=50, main=expression(paste("ACGS: ACF for ", beta)[1][7]), lwd=3, cex = 2, cex.lab = 1.5, cex.main = 1.5)
 dev.off()
 
 
